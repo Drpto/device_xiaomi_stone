f724d63308640a09694d7294760e341673b26acd
Drpto <dr.juniornoob@gmail.com>
2023-10-20 22:56:57 +0530

    stone: Track Viper4FX from https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,12 @@
 git clone --depth=1 https://github.com/Stone-Trees/device_xiaomi_sm6375-common.git -b lineage-20 device/xiaomi/sm6375-common
 
-git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git vendor/xiaomi
+git clone --depth=1 https://github.com/Stone-Trees/vendor_xiaomi.git -b lineage-20 vendor/xiaomi
 
 git clone --depth=1 https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b lineage-20 kernel/xiaomi/sm6375
 
 git clone --depth=1 https://github.com/Stone-Trees/xiaomi_stone-kernel.git device/xiaomi/stone-kernel
 
 git clone --depth=1 https://github.com/stone-devs/hardware_xiaomi hardware/xiaomi
+
+git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
+
