408c34dcfaa1d08288ab22319db8ce8b3e4912a0
Drpto <dr.juniornoob@gmail.com>
2023-10-26 23:58:58 +0530

    stone: Welcome to EvolutionX
    Signed-off-by: Drpto <dr.juniornoob@gmail.com>

--- evolution_stone.mk
+++ evolution_stone.mk
@@ -9,10 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from stone device
 $(call inherit-product, device/xiaomi/stone/device.mk)
+
+#EvolutionX prop
+EVO_BUILD_TYPE := UNOFFICIAL
 
 # Device props
 TARGET_SUPPORTS_BLUR := true
@@ -23,7 +26,7 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_USE_PIXEL_CHARGER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := evolution_stone
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
